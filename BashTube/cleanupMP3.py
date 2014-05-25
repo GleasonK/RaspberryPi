c23a20a0d199e9b5e2cd03172541541d99689638
Kevin Gleason <KAAGleason@gmail.com>
2014-05-25 20:45:00 +0000

    Bug fixes

--- BashTube/cleanupMP3.py
+++ BashTube/cleanupMP3.py
@@ -1,9 +1,21 @@
 import os
 
 # echo $(($(( $(date +%s)-$(date +%s --date=\"$(stat * | grep -m2 Access | sed '1d' | sed 's/Access: //g')\")))/60))
-path = "~/Music/YTDL/"
-files = os.popen("ls \'" + path + "'").read().rsplit()
-for fname in files:
-	# time = int(os.popen("echo $(($(( $(date +%s)-$(date +%s --date=\"$(stat "+fname+" | grep -m2 Access | sed '1d' | sed 's/Access: //g')\")))/60))").read())
-	if int(os.popen("echo $(($(( $(date +%s)-$(date +%s --date=\"$(stat \'"+path+fname+"\' | grep -m2 Access | sed '1d' | sed 's/Access: //g')\")))/86400))").read()) > 5:
-		os.system('rm '+path+fname)
+
+# Clean old files
+def clean_files(path):
+    files = os.popen("ls " + path).read().rsplit('\n')
+    files.__delitem__(len(files)-1)
+    print files
+    for fname in files:
+        if int(os.popen("echo $(($(( $(date +%s)-$(date +%s --date=\"$(stat \""+path+fname+"\" | grep -m2 Access | sed '1d' | sed 's/Access: //g')\")))/86400))").read()) > 5:
+            os.system('rm "'+path+fname+'"')
+
+
+if __name__ == '__main__':
+    # Set paths
+    paths = ["/home/pi/Music/YTDL/", "/home/pi/Music/PyBox/"]
+    
+    # Clean paths
+    for path in paths:
+        clean_files(path)
